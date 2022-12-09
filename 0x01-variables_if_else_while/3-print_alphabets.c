c335d62319da92618f1a17703bca24df95020ed8
iamjoee <joeadvocates123@gmail.com>
2022-12-09 09:23:11 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program returns alphabets both lower and upper cases'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+	int CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+	putchar(CH);
+	CH++;
+}
+putchar('\n');
+return (0);
+}
+
