ba83146affa10408efd5709edd5666ab84904870
iamjoee <joeadvocates123@gmail.com>
2022-12-14 04:01:44 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,9 +14,9 @@ void times_table(void)
 			c = a * b;
 			if ((c / 10) == 0)
 			{
-		if (b == 0)
+				if (b == 0)
 				{
-				     _putchar('0');
+					_putchar('0');
 				}
 				if (b != 0)
 				{
@@ -27,22 +27,20 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
-									}
+				}
 			}
-		}
-	else
-		{
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
-			if (b < 9)
+			else
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+
+				}
 			}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
-}
-
-
