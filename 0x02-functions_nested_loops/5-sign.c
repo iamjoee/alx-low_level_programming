256b6c50dff54ae1114ae5a199a816e7ee6fa52b
iamjoee <joeadvocates123@gmail.com>
2022-12-14 01:21:54 -0800

    isalpha

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * print_sign - return 0 letter not lowercase, 1 letter lowercase
- *  @n : number to check
- *  Return: 0 or 1
+ * @n : number to check
+ * Return: 0 or 1
  */
 int print_sign(int n)
 {
