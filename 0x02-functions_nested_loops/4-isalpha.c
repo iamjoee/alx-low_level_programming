2cd844ad946fd7cf49b7bebc9917307894806978
iamjoee <joeadvocates123@gmail.com>
2022-12-14 00:59:43 -0800

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 include "main.h"
 /**
- * _isalpha - check if a char is lowercase
+ * _isalpha - checks for char alphabet
  * @c: unsigned int value to be compared with ASCII value
  * Return: 1 if its alphabet otherwise 0
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if (c >= 65 && c <= 122)
 		return (1);
 	else
 		return (0);
