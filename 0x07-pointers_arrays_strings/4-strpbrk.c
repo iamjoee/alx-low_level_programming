9e99ca6e9d9b963a134ca7eb1d26bb941c963280
iamjoee <joeadvocates123@gmail.com>
2023-01-03 11:17:27 -0800

    4. strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strpbrk - function that searches a string
+ * for any of a set of bytes
+ * @s: string to search
+ * @accept: target matches
+ * Return: pointer to index of string at first occurence
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
